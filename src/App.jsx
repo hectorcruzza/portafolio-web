c2d3f67874ded6cceebcf1335b6998785730a51c
Héctor Cruz <hectorivancruzalayola@gmail.com>
2025-07-24 18:01:34 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <div class="flex flex-row">
+      <div class="flex-auto bg-blue-500">
+        <h1 class="text-2xl font-bold underline decoration-[#e28743]">
+          Hello World!
+        </h1>
+      </div>
+      <div class="flex-auto bg-green-500">
+        <h1 class="text-2xl font-bold underline decoration-[#e28743]">
+          Hello World!
+        </h1>
+      </div>
+      <div class="flex-auto bg-red-500">
+        <h1 class="text-2xl font-bold underline decoration-[#e28743]">
+          Hello World!
+        </h1>
+      </div>
+    </div>
+  );
+}
